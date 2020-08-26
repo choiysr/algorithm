5fe2b0bfca697c71ac385f74a7ef9eee14d8b380
choiysr <valsa_de_euridice@naver.com>
2020-08-26 19:43:19 +0900

    Initial commit

---
+++ src/Boj2309.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/* 백준 2309 : 일곱난쟁이 */
+public class Boj2309 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] numArr = new int[9];
+        int sum = 0;
+
+        for (int i = 0; i < 9; i++) {
+            numArr[i] = Integer.parseInt(br.readLine());
+            sum += numArr[i];
+        }
+
+        Loop : for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if(i!=j && (sum - (numArr[i] + numArr[j])==100)) {
+                    numArr[i] = 0;
+                    numArr[j] = 0;
+                    break Loop;
+                }
+            }
+        }
+
+        Arrays.sort(numArr);
+
+        for(int i = 2; i < 9; i++) {
+            System.out.println(numArr[i]);
+        }
+
+    }
+}
