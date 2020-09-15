2efb66ed4854faf8522313efda97eec486f7458b
dreamer <valsa_de_euridice@naver.com>
2020-09-15 17:37:25 +0900

    boj3085 - 틀림

--- src/Brute_force_Search/test.java
+++
@@ -1,9 +0,0 @@
-package Brute_force_Search;
-
-public class test {
-
-    public static void main(String[] args) {
-        int org = 9;
-        System.out.println(Math.pow(10,(int)Math.floor(Math.log10(org))));
-    }
-}
