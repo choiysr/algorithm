0759b7003ce3323d9f98b7ad647ad9b417b74f1f
choiysr <valsa_de_euridice@naver.com>
2020-10-25 23:15:05 +0900

    20201025 1

--- src/Brute_force_Search/Boj1182.java
+++ src/Brute_force_Search/Boj1182.java
@@ -1,48 +1,55 @@
 package Brute_force_Search;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 /*
- * Date : 20. 10. 22
+ * Date : 20. 10. 25
  * Question.No : 백준 1182 - 부분수열의 합
- * Remark : 틀림!!!!!!!!!!!!!!!
+ * Remark : 보고 풀었고 이해 못함.
  */
 public class Boj1182 {
 
-    public static void main(String[] args) throws IOException {
+    private static int count = 0;
+    private static int n, s;
+    private static int[] arr;
+
+    public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int s = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n];
-        String[] inputArr = br.readLine().split(" ");
-        int count = 0;
+        n = Integer.parseInt(st.nextToken());
+        s = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(inputArr[i]);
-            if(arr[i] == s) {
-                count++;
-            }
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i < n; i++) {
-            int sum = arr[i];
-            for (int j = i+1; j < n; j++) {
-                if((j!=n-1 && sum + arr[j] + arr[j+1] == s) || (j==n-1 && sum+arr[j]==s)) {
-                    sum+=arr[j];
-                    count++;
-                } else {
-                    sum += arr[j];
-                }
-            }
-        }
+        powerSet(0,0);
+
+        if(s == 0) count--;
 
         System.out.println(count);
 
+    }
+
+    private static void powerSet(int idx, int sum) {
+
+        System.out.println("idx = "+idx+","+"sum = "+sum);
+
+        if (idx == n) {
+            if (sum == s) {
+                count++;
+            }
+            return;
+        }
+
+        powerSet(idx+1, sum);
+        powerSet(idx+1, sum+arr[idx]);
 
     }
 }
---
+++ src/Brute_force_Search/Boj1182_2.java
@@ -0,0 +1,25 @@
+package Brute_force_Search;
+
+/*
+ * Date : 20. 10. 25
+ * Question.No : 백준 1182 - 부분수열의 합 연습
+ * Remark : 백트래킹 연습
+ */
+public class Boj1182_2 {
+
+    public static void main(String[] args) {
+
+        int n = 3;
+        int s = 6;
+        int[] arr = {1,2,3};
+        boolean[] check = new boolean[3];
+
+    }
+
+    private static void backTracking(int index, int sum) {
+
+
+        
+
+    }
+}
