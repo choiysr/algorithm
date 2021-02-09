187c4f50a84dd3a901859f4f1d483f155204f20b
YESORA <valsa_de_euridice@naver.com>
2021-02-09 09:44:57 +0900

    210209

--- src/Brute_force_Search/Boj1018_3.java
+++ src/Brute_force_Search/Boj1018_3.java
@@ -5,19 +5,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 /*
- * Date : 21. 02. 08
+ * Date : 21. 02. 09
  * Question.No : 백준 1018 - 체스판 다시 칠하기
- * Remark : 틀림. 다시해야함
+ * Remark :
  */
 public class Boj1018_3 {
 
     private static char[][] board;
     private static int N, M;
+    private static final int SIZE = 8;
 
     public static void main(String[] args) {
 
         // 하나만 구해서 64에서 뺀 후 둘 중 작은 수가 min
-        int minValue = 64;
+        int minValue = SIZE*SIZE;
 
         try {
             setOrgBoardByInput();
@@ -26,9 +27,9 @@ public class Boj1018_3 {
         }
 
         for (int i = 0; i < N; i++) {
-            if (i <= N - 8) {
+            if (i <= N - SIZE) {
                 for (int j = 0; j < M; j++) {
-                    if(j <= M - 8) {
+                    if(j <= M - SIZE) {
                         minValue = Math.min(minValue,getHowManyNodesMustChange(i, j));
                     }
                 }
@@ -50,7 +51,6 @@ public class Boj1018_3 {
         for (int i = 0; i < N; i++) {
             String eachLine = br.readLine();
             for (int j = 0; j < M; j++) {
-                System.out.println(i+","+j);
                 board[i][j] = eachLine.charAt(j);
             }
         }
@@ -60,8 +60,8 @@ public class Boj1018_3 {
 
         int count = 0;
         // 합이 짝수면 B, 홀수면 W
-        for (int i = startX; i < 8; i++) {
-            for (int j = startY; j < 8; j++) {
+        for (int i = startX; i < startX+SIZE; i++) {
+            for (int j = startY; j < startY+SIZE; j++) {
                 // 합이 짝수인데 board에서 B가 아니거나, 합이 홀수인데 board에서 W가 아닐 때 count++
                 if (((0 == ((i + j) % 2)) && (board[i][j] != 'B')) || ((0 != ((i + j) % 2)) && (board[i][j] == 'B'))) {
                     count++;
@@ -69,7 +69,7 @@ public class Boj1018_3 {
             }
         }
 
-        return Math.min(64 - count, count);
+        return Math.min((SIZE*SIZE) - count, count);
 
     }
 
