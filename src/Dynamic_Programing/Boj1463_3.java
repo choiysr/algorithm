979c03db7f0fecae84be2feb11c0d38a746219fd
YESORA <valsa_de_euridice@naver.com>
2021-02-24 17:22:07 +0900

    210224

--- src/Dynamic_Programing/Boj1463_3.java
+++ src/Dynamic_Programing/Boj1463_3.java
@@ -1,13 +1,53 @@
 package Dynamic_Programing;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /*
- * Date : 21. 02. 19
+ * Date : 21. 02. 23
  * Question.No : 백준 1463 - 1로 만들기
- * Remark :
+ * Remark : ***************************틀림. 미완성. 수정해야함
  */
 public class Boj1463_3 {
 
-    public static void main(String[] args) {
+    private static int[][] arr;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        if(n==1 || n==2 || n==3) {
+            System.out.println(n/2);
+            return;
+        }
+
+        arr = new int[n+1][3];
+        arr[2][1] = 1;
+        arr[3][0] = 1;
+
+        for (int i = 4; i < n + 1; i++) {
+
+            int min = Integer.MAX_VALUE;
+
+            if (i % 2 == 0 && i % 3 == 0) {
+                arr[i][0] = arr[3][0] + arr[i/3][0];
+                arr[i][1] = arr[i / 3][1];
+            } else if (i % 3 == 0) {
+                arr[i][0] = arr[3][0] + arr[i/3][0];
+            } else if (i % 2 == 0) {
+                arr[i][1] = Math.min(arr[i-1][0]+arr[i-1][1]+arr[i-1][2]+1,arr[2][1] + arr[i/2][1]);
+            } else {
+                arr[i][0] = arr[i - 1][0];
+                arr[i][1] = arr[i - 1][1];
+                arr[i][2] = arr[i - 1][2] + 1;
+            }
+        }
+
+        System.out.println(arr[n][0] + arr[n][1] + arr[n][2]);
+
+
 
     }
 }
---
+++ src/Dynamic_Programing/Boj2839.java
@@ -0,0 +1,33 @@
+package Dynamic_Programing;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+ * Date : 21. 02. 24
+ * Question.No : 백준 2839 - 설탕배달
+ * Remark : DP로 풀어야 하는뎅...ㅠㅠ
+ */
+public class Boj2839 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sugar = Integer.parseInt(br.readLine());
+        int mod5 = sugar % 5;
+
+        if (4 == sugar || 7 == sugar) {
+            System.out.println(-1);
+        } else if (0 == mod5) {
+            System.out.println(sugar / 5);
+        } else if (1 == mod5 || 3 == mod5) {
+            System.out.println(sugar / 5 + 1);
+        } else if (2 == mod5 || 4 == mod5) {
+            System.out.println(sugar / 5 + 2);
+        } else {
+            System.out.println(sugar / 3);
+        }
+
+    }
+}
