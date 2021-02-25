f2a28e384bd8a516879efc3fcbe9e4ecfa8fe6a2
YESORA <valsa_de_euridice@naver.com>
2021-02-25 15:27:44 +0900

    210225

--- src/Dynamic_Programing/Boj1463_3.java
+++ src/Dynamic_Programing/Boj1463_3.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 /*
  * Date : 21. 02. 23
  * Question.No : 백준 1463 - 1로 만들기
- * Remark : ***************************틀림. 미완성. 수정해야함!!!!!!!!!!!!!2/25이후로 꼭 수정할것 
+ * Remark : 맞았습니다
  *
 570 // 정답: 8
 571 // 정답: 9
@@ -38,62 +38,51 @@ public class Boj1463_3 {
 
         for (int i = 4; i < n + 1; i++) {
 
-            if(i==570);
-
             if (i % 2 == 0 && i % 3 == 0) {
-                arr[i][0] = arr[i / 3][0] + 1;
-                arr[i][1] = arr[i / 3][1];
-                arr[i][2] = arr[i / 3][2];
-            } else if (i % 3 != 0) {
-                // -1을 했을 경우 값
-                int first = arr[i - 1][0] + arr[i - 1][1] + arr[i - 1][2] + 1;
-                // 2로 나눠서 나온 값과 비교
-                int second = arr[i / 2][0] + arr[i / 2][1] + arr[i / 2][2] + 1;
-                if (first < second) {
-                    arr[i][0] = arr[i - 1][0];
-                    arr[i][1] = arr[i - 1][1];
-                    arr[i][2] = arr[i - 1][2] + 1;
+                // 2로 나누는 경우와 3으로 나누는 경우를 비교.
+                if (getElementByIndex(i,2) < getElementByIndex(i,3)) {
+                    setArray(i, 2);
                 } else {
-                    arr[i][0] = arr[i / 2][0];
-                    arr[i][1] = arr[i / 2][1] + 1;
-                    arr[i][2] = arr[i / 2][2];
+                    setArray(i, 3);
+                }
+            } else if (i % 2 == 0) {
+                // 1을 빼는 경우와 2로 나누는 경우를 비교.
+                if (getElementByIndex(i,1) < getElementByIndex(i,2)){
+                    setArray(i, 1);
+                } else {
+                    setArray(i, 2);
                 }
             } else if (i % 3 == 0) {
-                // -1을 했을 경우 값
-                int first = arr[i - 1][0] + arr[i - 1][1] + arr[i - 1][2] + 1;
-                // 3으로로 나눠서 나온 값과 비교
-                int second = arr[i / 3][0] + arr[i / 3][1] + arr[i / 3][2] + 1;
-                if (first < second) {
-                    arr[i][0] = arr[i - 1][0];
-                    arr[i][1] = arr[i - 1][1];
-                    arr[i][2] = arr[i - 1][2] + 1;
+                if (getElementByIndex(i,1) < getElementByIndex(i,3)) {
+                    setArray(i, 1);
                 } else {
-                    arr[i][0] = arr[i / 3][0] + 1;
-                    arr[i][1] = arr[i / 3][1];
-                    arr[i][2] = arr[i / 3][2];
+                    setArray(i, 3);
                 }
             } else {
-                arr[i][0] = arr[i - 1][0];
-                arr[i][1] = arr[i - 1][1];
-                arr[i][2] = arr[i - 1][2] + 1;
+                setArray(i, 1);
             }
 
-/*            if (i % 2 == 0 && i % 3 == 0) {
-                arr[i][0] = arr[3][0] + arr[i/3][0];
-                arr[i][1] = arr[i / 3][1];
-            } else if (i % 3 == 0) {
-                arr[i][0] = arr[3][0] + arr[i/3][0];
-            } else if (i % 2 == 0) {
-                arr[i][1] = Math.min(arr[i-1][0]+arr[i-1][1]+arr[i-1][2]+1,arr[2][1] + arr[i/2][1]);
-            } else {
-                arr[i][0] = arr[i - 1][0];
-                arr[i][1] = arr[i - 1][1];
-                arr[i][2] = arr[i - 1][2] + 1;
-            }*/
         }
 
         System.out.println(arr[n][0] + arr[n][1] + arr[n][2]);
 
+    }
+
+    public static int getElementByIndex(int index, int computingTarget) {
+
+        int target = computingTarget == 3 ? index / 3 : computingTarget == 2 ? index / 2 : index - 1;
+        return arr[target][0] + arr[target][1] + arr[target][2];
 
     }
+
+    public static void setArray(int index, int computingTarget) {
+
+        int target = computingTarget == 3 ? index / 3 : computingTarget == 2 ? index / 2 : index - 1;
+
+        for (int i = 0; i < 3; i++) {
+            arr[index][i] = computingTarget + i == 3 ? arr[target][i]+1 : arr[target][i];
+        }
+
+    }
+
 }
